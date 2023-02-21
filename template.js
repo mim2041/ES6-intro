b6103728fe49bd92f077b83f91246ebdb3e4c629
mim2041 <suraiyamim338@gmail.com>
2023-02-21 21:36:15 +0600

    overview of templete string, arrow function

---
+++ template.js
@@ -0,0 +1,22 @@
+const person = 'Adam sand';
+const person2 = 'Ben white';
+const person3 = `Donald Trump`;
+
+const multiline = 'First Line Text \n' + 
+'second line of code' + 
+'third line of code';
+// console.log(multiline);
+
+const newMultiline = `First Line of text
+second line of text
+third line of code`;
+// console.log(newMultiline);
+
+const a=20;
+const b=30;
+const nums = [33, 34, 55,43,32];
+// const summery = 'sum of: '+a + 'and' +b ,'is: '+ (a+b);
+console.log(summery);
+
+const newSummery = `sum of ${a} and ${b} is ${a+b}`;
+console.log(newSummery);
